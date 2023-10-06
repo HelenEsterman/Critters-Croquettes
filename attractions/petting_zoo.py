5a306ae2c201e787cfa9c6d2b9c82412f33dd001
Helen Esterman <helenesterman99@gmail.com>
2023-10-06 15:59:15 -0400

    added classes for attraction types

---
+++ attractions/petting_zoo.py
@@ -0,0 +1,10 @@
+class PettingZoo:
+
+    def __init__(self, attraction_name, description):
+
+        self.attraction_name = attraction_name
+        self.description = description
+        self.animals = list()
+
+    def add_animal(self):
+        return self.animals.append(self)
---
+++ attractions/wetlands.py
@@ -0,0 +1,10 @@
+class Wetlands:
+
+    def __init__(self, attraction_name, description):
+
+        self.attraction_name = attraction_name
+        self.description = description
+        self.animals = list()
+
+    def add_animal(self):
+        return self.animals.append(self)
